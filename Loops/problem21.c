a3a66bcb78098a4460f9cc488b5f004e60148741
shivansh-Dseu <shivanshkumar.8595@gmail.com>
2026-08-29 19:26:37 +0530

    Add loop problems and updates

--- Loops/problem21.c
+++ Loops/problem21.c
@@ -1,1 +1,21 @@
-// print all armstrong numbers between 1 and 500.
+// print all alphabet and their correspondings ASCII values.
+
+#include <stdio.h>
+
+int main()
+{
+    char CH = 'A';
+    char ch = 'a';
+    
+    while (ch <= 'z' && CH <= 'Z')
+    {
+        printf("%c -> %d and %c -> %d\n", CH, CH, ch, ch);
+        ch++;
+        CH++;
+    }
+
+    return 0;
+}
+
+
+
